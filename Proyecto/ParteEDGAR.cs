784d8db44d3632250b1be9fa9014b96bc62620d3
Ramiro Valdovinos <edgaritoram2@gmail.com>
2025-07-07 10:21:05 -0600

    creacion de rama

--- Proyecto/ParteEDGAR.cs
+++ Proyecto/ParteEDGAR.cs
@@ -2,6 +2,24 @@
 {
     public class ParteEDGAR
     {
+        public float velsosidad;
+        public void inicio()
+        {
+            Console.WriteLine("Inicio del movimiento.");
+        }
+        public void Mover1()
+        {
+            velsosidad = 10.0f;
+            Console.WriteLine("La velocidad es: " + velsosidad);
+        }
+        public void Detener()
+        {
+            velsosidad = 0.0f;
+            Console.WriteLine("La velocidad disminulle.");
+            Console.WriteLine("Se a detenido");
+
+        }
        
+
     }
 }
