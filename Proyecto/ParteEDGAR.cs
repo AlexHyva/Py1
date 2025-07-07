202b5d57bad06cea30d3872f4b998dca5c1038ce
JAIRO ALEJANDRO VALDOVINOS DAMIAN <22460706@colima.tecnm.mx>
2025-07-07 10:35:43 -0600

    agrege tectos sobre  las accioness que realizara la banda ensu funcionamiento

--- Proyecto/ParteEDGAR.cs
+++ Proyecto/ParteEDGAR.cs
@@ -1,7 +1,25 @@
 ﻿namespace Proyecto
 {
-    public class ParteEDGAR
+    public class ParteJairo
     {
-       
+        public float velocidad;
+        public void inicio()
+        {
+            Console.WriteLine("Inicio del movimiento de la banda.");
+        }
+        public void Mover1()
+        {
+            velocidad = 10.0f;
+            Console.WriteLine("La banda esta en movimiento: " + velocidad);
+        }
+        public void Detener()
+        {
+            velocidad = 0.0f;
+            Console.WriteLine("La velocidad  esta disminullendo gradualmente.");
+            Console.WriteLine("Se a detenido por completo la banda");
+
+        }
+
+
     }
 }
