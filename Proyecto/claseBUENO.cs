c37d74c71529f1973448cc94df77fb34da45330f
AlexHyva <abj1717@gmail.com>
2025-07-07 10:49:05 -0600

    agregue una barra de seleccion de direccion y  cantidad de cajas

--- Proyecto/claseBUENO.cs
+++ Proyecto/claseBUENO.cs
@@ -3,12 +3,20 @@
 
     //2.-Condicionar el objeto para poder moverlo a diferentes direcciones así como
     //agregar o eliminar el mismo objeto para aumentar la cantidad del mismo
+   
 
     public class claseBUENO
     {
+        public float cajas;
+        public int direccion;
+
         public void Mover()
         {
-            Console.WriteLine("Hola, soy la clase BUENO");
+            Console.WriteLine("Elije la direccion");
+            Console.WriteLine("Izquierda(1) o Derecha(0)");
+            int direccion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Elije la cantidad de cajas");
+            int caja = int.Parse(Console.ReadLine());
         }
 
 
