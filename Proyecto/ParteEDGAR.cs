ee2a2e0ce94f93c4699327da023d1edac478641a
Ramiro Valdovinos <edgaritoram2@gmail.com>
2025-07-07 10:41:21 -0600

    cambios en las impresiones

--- Proyecto/ParteEDGAR.cs
+++ Proyecto/ParteEDGAR.cs
@@ -12,14 +12,9 @@
             velsosidad = 10.0f;
             Console.WriteLine("La velocidad es: " + velsosidad);
         }
-        public void Detener()
-        {
-            velsosidad = 0.0f;
-            Console.WriteLine("La velocidad disminulle.");
-            Console.WriteLine("Se a detenido");
+      
+        
 
-        }
-       
 
     }
 }
